a26383fcc05e8593d623be879f40256e1a5ae18c
crazylyf <crazylyf@163.com>
2023-01-19 15:59:37 +0800

    add micro_environment analyze scripts

---
+++ yufeng/micro_environ/micro_env_features.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+#================================================================
+#   Copyright (C) 2023 Yufeng Liu (Braintell, Southeast University). All rights reserved.
+#   
+#   Filename     : micro_env_features.py
+#   Author       : Yufeng Liu
+#   Date         : 2023-01-18
+#   Description  : 
+#
+#================================================================
+import time
+import numpy as np
+import pandas as pd
+from scipy.spatial import distance_matrix
+
+class MEFeatures:
+    __FEAT_NAMES__ = [
+        'Nodes', 'SomaSurface', 'Stems', 'Bifurcations',
+        'Branches', 'Tips', 'OverallWidth', 'OverallHeight', 'OverallDepth',
+        'AverageDiameter', 'Length', 'Surface', 'Volume',
+        'MaxEuclideanDistance', 'MaxPathDistance', 'MaxBranchOrder',
+        'AverageContraction', 'AverageFragmentation',
+        'AverageParent-daughterRatio', 'AverageBifurcationAngleLocal',
+        'AverageBifurcationAngleRemote', 'HausdorffDimension']
+    __FEAT_ALL__ = [
+        'Nodes_mean', 'SomaSurface_mean', 'Stems_mean', 'Bifurcations_mean', 
+        'Branches_mean', 'Tips_mean', 'OverallWidth_mean', 'OverallHeight_mean', 
+        'OverallDepth_mean', 'AverageDiameter_mean', 'Length_mean', 'Surface_mean', 
+        'Volume_mean', 'MaxEuclideanDistance_mean', 'MaxPathDistance_mean', 
+        'MaxBranchOrder_mean', 'AverageContraction_mean', 'AverageFragmentation_mean', 
+        'AverageParent-daughterRatio_mean', 'AverageBifurcationAngleLocal_mean', 
+        'AverageBifurcationAngleRemote_mean', 'HausdorffDimension_mean',
+        'Nodes_median', 'SomaSurface_median', 'Stems_median', 'Bifurcations_median', 
+        'Branches_median', 'Tips_median', 'OverallWidth_median', 'OverallHeight_median', 
+        'OverallDepth_median', 'AverageDiameter_median', 'Length_median', 'Surface_median', 
+        'Volume_median', 'MaxEuclideanDistance_median', 'MaxPathDistance_median', 
+        'MaxBranchOrder_median', 'AverageContraction_median', 'AverageFragmentation_median', 
+        'AverageParent-daughterRatio_median', 'AverageBifurcationAngleLocal_median', 
+        'AverageBifurcationAngleRemote_median', 'HausdorffDimension_median',
+        'Nodes_std', 'SomaSurface_std', 'Stems_std', 'Bifurcations_std', 
+        'Branches_std', 'Tips_std', 'OverallWidth_std', 'OverallHeight_std', 
+        'OverallDepth_std', 'AverageDiameter_std', 'Length_std', 'Surface_std', 
+        'Volume_std', 'MaxEuclideanDistance_std', 'MaxPathDistance_std', 
+        'MaxBranchOrder_std', 'AverageContraction_std', 'AverageFragmentation_std', 
+        'AverageParent-daughterRatio_std', 'AverageBifurcationAngleLocal_std', 
+        'AverageBifurcationAngleRemote_std', 'HausdorffDimension_std'
+        ]
+
+    def __init__(self, feature_file, region_num='316'):
+        self.region_num = region_num    # not the actual number of regions
+        self.df = self.load_features(feature_file)
+
+    def load_features(self, feature_file, remove_nan=True, min_num_recons=20):
+        df = pd.read_csv(feature_file, index_col=0)
+        print(f'Initial number of recons: {df.shape[0]}')
+        if remove_nan:
+            err_f = ~(df.isna().sum(axis=1).astype(np.bool))
+            df = df[err_f]
+        print(f'Number after remove_nan: {df.shape[0]}')
+
+        # keep regions with at leat `min_num_recons` neurons
+        rkey = f'region_id_r{self.region_num}'
+        regions, counts = np.unique(df[rkey], return_counts=True)
+        abd_regions = regions[counts >= min_num_recons]
+        df = df[df[rkey].isin(abd_regions)]
+        print(f'Number after removing regions less thant {min_num_recons}: {df.shape[0]}')
+
+        return df
+
+    def calc_micro_env_features(self, mefeature_file, topk=5):
+        # we should pre-normalize each feature for topk extraction
+        df = self.df.copy()
+        feats = df.loc[:, self.__FEAT_NAMES__]
+        feats = (feats - feats.mean()) / (feats.std() + 1e-10)
+        df[self.__FEAT_NAMES__] = feats
+
+        # Use the statistical features of the topk of neurons with the same region
+        rkey = f'region_id_r{self.region_num}'
+        regions = np.unique(df[rkey])
+        fdim = len(self.__FEAT_NAMES__)
+        df_mef = self.df.copy() #df.drop(self.__FEAT_NAMES__, axis=1)
+        
+        df_mef[self.__FEAT_ALL__] = 0
+        print(df_mef.shape)
+        t0 = time.time()
+        for region in regions:
+            print(f'==> Processing region: {region}')
+            # all neurons in current region
+            region_mask = df[rkey] == region
+            region_index = df_mef.index[region_mask]
+            # pairwise distance estimation
+            feat = df.loc[region_index, self.__FEAT_NAMES__]
+            pdist = distance_matrix(feat, feat)
+            indices_topk = np.argpartition(pdist, topk+1, axis=1)[:, :topk+1]
+            # get all features at once
+            mef_raw = self.df.loc[region_index, self.__FEAT_NAMES__].iloc[indices_topk.reshape(-1)].to_numpy().reshape(-1, topk+1, fdim)
+            mef_mean = mef_raw.mean(axis=1)
+            mef_median = np.median(mef_raw, axis=1)
+            mef_std = mef_raw.std(axis=1)
+            # micro_env_features
+            mef = np.stack((mef_mean, mef_median, mef_std), axis=1).reshape((mef_mean.shape[0],-1))
+            df_mef.loc[region_index, self.__FEAT_ALL__] = mef
+        print(f'--> Total time used: {time.time() - t0:.2f} sec')
+
+        # normalize by removing background
+        feats = df_mef.loc[:, self.__FEAT_ALL__]
+        df_mef.loc[:, self.__FEAT_ALL__] = (feats - feats.mean()) / (feats.std() + 1e-10)
+
+        df_mef.to_csv(mefeature_file, float_format='%g')
+
+    def calc_normalized_regional_mefeatures(self, mefeature_file, nmefeature_file):
+        df = pd.read_csv(mefeature_file, index_col=0)
+        feats = df.loc[:, self.__FEAT_ALL__]
+        df.loc[:, self.__FEAT_ALL__] = (feats - feats.mean()) / (feats.std() + 1e-10)
+
+        df.to_csv(nmefeature_file, float_format='%g')
+
+if __name__ == '__main__':
+    feature_file = '../data/lm_features_d22_all.csv'
+    mefeature_file = '../data/micro_env_features_d66_all.csv'
+    nmefeature_file = '../data/micro_env_features_d66_all_normalized.csv'
+    mef = MEFeatures(feature_file)
+    #mef.calc_micro_env_features(mefeature_file)
+    mef.calc_normalized_regional_mefeatures(mefeature_file, nmefeature_file)
+        
---
+++ yufeng/micro_environ/preprocess.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+#================================================================
+#   Copyright (C) 2023 Yufeng Liu (Braintell, Southeast University). All rights reserved.
+#   
+#   Filename     : preprocess.py
+#   Author       : Yufeng Liu
+#   Date         : 2023-01-17
+#   Description  : 
+#
+#================================================================
+
+import os
+import glob
+import re
+import numpy as np
+import pandas as pd
+
+from file_io import load_image
+from anatomy.anatomy_config import MASK_CCF25_FILE
+from anatomy.anatomy_core import parse_ana_tree
+
+import sys
+sys.path.append('../../brain_statistics')
+from common_func import load_regions, get_region_mapper
+
+def add_r316_regions(df, ana_dict, mask_regions):
+    rids_set = load_regions(level=0)
+    orig_ids = mask_regions
+
+    rc_dict = get_region_mapper(rids_set, orig_ids, ana_dict, reverse_mapper=True)[1]
+    
+    new_rids = []
+    new_rnames = []
+    for idx in df['region_id_r671']:
+        if idx in rc_dict:
+            new_id = rc_dict[idx]
+            new_id = new_id
+            new_rids.append(new_id)
+            new_rname = ana_dict[new_id]['acronym']
+            new_rnames.append(new_rname)
+        else:
+            new_rids.append(0)
+            new_rnames.append(np.NaN)
+
+    df['region_id_r316'] = new_rids
+    df['region_name_r316'] = new_rnames
+    
+    return df
+        
+
+def aggregate_information(feature_dir, swc_dir):
+    """
+       Aggregate more information besides the 22d features, and combine all features into an
+     unified file. Additional information including: 
+       - soma_pos[x,y,z] in CCFv3 space
+       - registered regions in the finest granuity, that is 671
+       - name of regions
+       - brain id
+       - dataset name: 150k or 50k
+       - and original name and features
+    """
+    DEBUG = False
+
+    # soma positions
+    swcnames = []
+    file_infos = []
+    for ds in glob.glob(os.path.join(swc_dir, '*')):
+        dname = os.path.split(ds)[-1]
+        for brain_dir in glob.glob(os.path.join(ds, '[1-9]*[0-9]')):
+            brain_id = os.path.split(brain_dir)[-1]
+            if DEBUG:
+                if brain_id != '17051': continue
+            print(f'--> Loading soma position for {dname}/{brain_id}')
+            for swcfile in glob.glob(os.path.join(brain_dir, '*.swc')):
+                # find the soma postion from file with decoding
+                with open(swcfile) as fp:
+                    soma_str = re.search('.* -1\n', fp.read()).group()
+                spos = soma_str.split()[2:5]
+                swcnames.append(os.path.split(swcfile)[-1])
+                file_infos.append((*spos, dname, brain_id))
+    
+    df = pd.DataFrame(file_infos, columns=['soma_x', 'soma_y', 'soma_z', 'dataset_name', 'brain_id'],
+        index=swcnames
+    ).astype({'soma_x': np.float, 'soma_y': np.float, 'soma_z': np.float})
+    print(df.dtypes)
+    
+    # get the region
+    print('>> Extract the region id and name')
+    ana_dict = parse_ana_tree()
+    mask = load_image(MASK_CCF25_FILE)  # z,y,x order!
+    mask_regions = [idx for idx in np.unique(mask) if idx != 0]
+
+    sposs = np.round(df[['soma_x', 'soma_y', 'soma_z']].to_numpy()).astype(np.int)
+    # clip
+    np.clip(sposs, 0, np.array(mask.shape[::-1])-1, out=sposs)
+    # region accessing in vectorized style
+    region_ids = mask[sposs[:,2], sposs[:,1], sposs[:,0]]
+    region_names = [ana_dict[idx]['acronym'] if idx != 0 else np.NaN for idx in region_ids]
+
+    df['region_id_r671'] = region_ids
+    df['region_name_r671'] = region_names
+    # also get the r316 
+    add_r316_regions(df, ana_dict, mask_regions)
+
+    # also merge the L-measure features
+    print('===> Merge with the features')
+    fdim = 22
+    features = []
+    for ds in glob.glob(os.path.join(feature_dir, '*')):
+        dname = os.path.split(ds)[-1]
+        for brain_file in glob.glob(os.path.join(ds, '*.txt')):
+            brain_id = os.path.splitext(os.path.split(brain_file)[-1])[0]
+
+            if DEBUG:
+                if brain_id != '17051': continue
+            print(f'======> parsing feature for {dname}/{brain_id}')
+            brain_features = pd.read_csv(brain_file, index_col=0)
+            features.append(brain_features)
+    features = pd.concat(features, join='inner').astype(np.float)
+    # concat with the overall information
+    df = df.join(features, how='inner')
+
+    df.to_csv('lm_features_d22_all.csv', float_format='%g')
+    
+    
+    
+
+
+if __name__ == '__main__':
+    swc_dir = '/PBshare/SEU-ALLEN/Users/yfliu/transtation/Research/platform/micro_environ/data/42k_local_morphology_gcoord_registered'
+    feature_dir = '/PBshare/SEU-ALLEN/Users/yfliu/transtation/Research/platform/micro_environ/data/42k_vaa3d_feature'
+    aggregate_information(feature_dir, swc_dir)
+
+    
