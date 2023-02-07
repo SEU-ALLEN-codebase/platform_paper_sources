6e1077c0b71ececcfee19f997ee98e67c587ade1
crazylyf <crazylyf@163.com>
2023-02-07 09:22:38 +0800

    improved figure layout

--- yufeng/sd_features/src/plot_features.py
+++ yufeng/sd_features/src/plot_features.py
@@ -19,11 +19,13 @@ from matplotlib.patches import Patch
 import seaborn as sns
 
 sys.path.append('../../common_lib')
-from common_utils import struct_dict, CorticalLayers, PstypesToShow
+from common_utils import struct_dict, CorticalLayers, PstypesToShow, normalize_features
 
 def rename_func(x, key=''):
     x = x.replace('_', ' ').split(' ')
     x = ''.join([xi[0].upper() + xi[1:] for xi in x])
+    x = x.replace('Average', 'Avg')
+    x = x.replace('Bifurcation', 'Bif')
     
     return x + key
 
@@ -32,6 +34,12 @@ def rename_mean(x):
 
 def rename_std(x):
     return rename_func(x, 'Std')
+
+def rename_mean_mv(x):
+    return rename_func(x)[:-4]
+
+def rename_std_mv(x):
+    return rename_func(x)[:-3]
 
 def get_stype_categories():
     index = struct_dict['CTX'] + struct_dict['TH'] + struct_dict['STR']
@@ -70,6 +78,8 @@ for ct in ['stype', 'ptype', 'cstype']:
     print(f'--> {ct}')
     df_feats = pd.DataFrame([])
     ltypes = []
+    fdims = []
+    cur_levels = []
     for level in levels:
         print(f'level: {level}')
         mfile = os.path.join(data_dir, f'{ct}_mean_features_{level}.csv')
@@ -85,10 +95,19 @@ for ct in ['stype', 'ptype', 'cstype']:
                 std_feat = pd.read_csv(sfile, index_col=0).rename(columns=rename_std)
             feats = pd.concat([mean_feat, std_feat], axis=1)
         else:
-            feats = pd.read_csv(mfile, index_col=0).rename(columns=rename_func)
+            if level == 'microenviron':
+                feats = pd.read_csv(mfile, index_col=0).rename(columns=rename_mean_mv)
+            else:
+                feats = pd.read_csv(mfile, index_col=0).rename(columns=rename_func)
 
         df_feats = pd.concat([df_feats, feats], axis=1)
         ltypes.extend([level for i in range(feats.shape[1])])
+        # record the feature dim and level for cur estimation
+        fdims.append(feats.shape[1])
+        cur_levels.append(level)
+
+    if ct == 'ptype':
+        df_feats.drop(['AId-Car3', 'CLA-Car3', 'SSs-Car3'], inplace=True)
 
     df_feats = df_feats.transpose()
     # customize row colors
@@ -130,27 +149,44 @@ for ct in ['stype', 'ptype', 'cstype']:
         #ax_row_colors.set_position([box_heatmap.max[0], box_row_colors.y0, box_row_colors.width*1.5, box_row_colors.height])
 
         # row legend
-        for label in rcolors_dict.keys():
-            g2.ax_row_dendrogram.bar(0, 0, color=rcolors_dict[label],
-                                    label=label, linewidth=0)
-        g2.ax_row_dendrogram.legend(title='Level', loc="upper right", ncol=1, 
-                                    bbox_to_anchor=(2., 1.28))
+        #for label in rcolors_dict.keys():
+        #    g2.ax_row_dendrogram.bar(0, 0, color=rcolors_dict[label],
+        #                            label=label, linewidth=0)
+        #g2.ax_row_dendrogram.legend(title='Level', loc="upper right", ncol=1, 
+        #                            bbox_to_anchor=(2., 1.28))
 
-        # column legend
+        # row colors
+        rc_fontsize = 15
+        nfdim = sum(fdims)
+        for fdim, cfdim, lev in zip(fdims, np.cumsum(fdims), cur_levels):
+            g2.ax_row_colors.axes.text(-0.9, 1- (cfdim - fdim/2.)/nfdim, lev[0].upper()+lev[1:], 
+                        ha='center', va='center', transform=g2.ax_row_colors.axes.transAxes,
+                        rotation=90., color='black', fontsize=rc_fontsize*3//2)
+        g2.ax_row_colors.axes.set_xticklabels(['Level'], fontsize=rc_fontsize)
+        # column colors
         for label in lut.keys():
             g2.ax_col_dendrogram.bar(0, 0, color=lut[label],
                                     label=label, linewidth=0)
-        g2.ax_col_dendrogram.legend(title=ct_label, loc="upper right", ncol=1, 
-                                    bbox_to_anchor=(0.9, 0.9))
+        g2_col_leg = g2.ax_col_dendrogram.legend(title=ct_label, loc="lower left", ncol=1, 
+                                    bbox_to_anchor=(1.01, 0.01), fontsize=rc_fontsize)
+        plt.setp(g2_col_leg.get_title(), fontsize=rc_fontsize)
 
-        # colorbar
-        g2.cax.set_position([0.9, .1, .03, .2])
-        g2.cax.set_ylabel('Standardized feature value')
-        g2.cax.set_yticks([-2,-1,0,1,2])
+        g2.ax_col_colors.axes.set_yticklabels(['s-type'], fontsize=rc_fontsize)
         
-        plt.savefig(f'{ct}_featuremap.png')
+        # colorbar
+        g2.cax.set_position([0.16, .82, .02, .15])
+        g2.cax.set_ylabel('Standardized feature value', fontsize=rc_fontsize)
+        g2.cax.set_yticks(np.arange(-2,3), np.arange(-2,3), fontsize=rc_fontsize)
+        g2.cax.tick_params(left=True, right=False, labelleft=True, labelright=False, direction='in')
+        g2.cax.yaxis.set_label_position("left")
+
+        # remove xlabel
+        g2.ax_heatmap.set_xlabel('', fontsize=0)
+        g2.ax_heatmap.tick_params(axis='x', labelsize=13, direction='in')
+        g2.ax_heatmap.tick_params(axis='y', labelsize=11, direction='in')
+        
+        plt.savefig(f'{ct}_featuremap.png', dpi=300)
         plt.close('all')
- 
 
 
     #------------- The following section is for data mining -------------------#
@@ -162,7 +198,8 @@ for ct in ['stype', 'ptype', 'cstype']:
         orders_x = np.zeros(indices_x.shape)
         for i in range(nfeatures):
             orders_x[i][indices_x[i]] = range(nclasses)
-        orders_x = np.fabs(orders_x - (nclasses+1)/2.)
+        orders_x_raw = orders_x - (nclasses+1)/2.
+        orders_x = np.fabs(orders_x_raw)
         # sort across the feature-level
         indices_y = np.argsort(orders_x, axis=0)
         orders_y = np.zeros(indices_y.shape)
@@ -172,23 +209,51 @@ for ct in ['stype', 'ptype', 'cstype']:
         df_orders = df_feats.copy()
         df_orders.iloc[:,:] = orders_y
         # visualization
-        #df_orders.clip(0, 10, inplace=True)  # show only the top10 
-        #df_prominence = 1 - df_orders / 10.
         df_prominence = (df_orders - df_orders.shape[0] + 11) / 10.
         df_prominence.clip(0, 1, inplace=True)
+        dfp = df_prominence.to_numpy()
+        mask = orders_x_raw < 0
+        dfp[mask] = dfp[mask] * -1.
+        df_prominence.iloc[:,:] = (dfp + 1.)/2
 
         # reorder the columns to keep it the same as clustermap before
         reordered_ind = g2.dendrogram_col.reordered_ind
         df_prominence = df_prominence.iloc[:, reordered_ind]
 
-        g1 = sns.clustermap(df_prominence, cmap='OrRd', row_colors=row_colors,
+        g1 = sns.clustermap(df_prominence, cmap='coolwarm', row_colors=row_colors,
                             col_colors=col_colors, row_cluster=False,
                             col_cluster=False, 
                             xticklabels=1, yticklabels=1,
-                            figsize=(10,20))
+                            figsize=(10,20), cbar_pos=(0.35, 0.85, 0.3, 0.01),
+                            cbar_kws=dict(orientation='horizontal'))
+
+        # row colors
+        rc_fontsize = 15
+        nfdim = sum(fdims)
+        for fdim, cfdim, lev in zip(fdims, np.cumsum(fdims), cur_levels):
+            g1.ax_row_colors.axes.text(-0.9, 1- (cfdim - fdim/2.)/nfdim, lev[0].upper()+lev[1:], 
+                        ha='center', va='center', transform=g1.ax_row_colors.axes.transAxes,
+                        rotation=90., color='black', fontsize=rc_fontsize*3//2)
+        g1.ax_row_colors.axes.set_xticklabels(['Level'], fontsize=rc_fontsize)
+        # column colors
+        for label in lut.keys():
+            g1.ax_col_dendrogram.bar(0, 0, color=lut[label],
+                                    label=label, linewidth=0)
+        g1_col_leg = g1.ax_col_dendrogram.legend(title=ct_label, loc="lower left", ncol=1, 
+                                    bbox_to_anchor=(1.01, 0.01), fontsize=rc_fontsize)
+        plt.setp(g1_col_leg.get_title(), fontsize=rc_fontsize)
+
+        g1.ax_col_colors.axes.set_yticklabels(['s-type'], fontsize=rc_fontsize)
+        
         # colorbar
-        g1.cax.set_position([0.9, .1, .03, .2])
-        g1.cax.set_ylabel('Feature prominence')
+        g1.cax.set_xlabel('Feature prominence', fontsize=rc_fontsize)
+        g1.cax.set_xticks(np.arange(0,1.001,0.5), np.arange(0,1.001,0.5), fontsize=rc_fontsize)
+        g1.cax.tick_params(direction='in')
+
+        # remove xlabel
+        g1.ax_heatmap.set_xlabel('', fontsize=0)
+        g1.ax_heatmap.tick_params(axis='x', labelsize=13, direction='in')
+        g1.ax_heatmap.tick_params(axis='y', labelsize=11, direction='in')
         
         plt.savefig(f'{ct}_prominence.png', dpi=300)
         plt.close('all')
